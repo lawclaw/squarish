1a0f2cfc583e124c4e8466dd7e2ad5b8f8355c90
Vinh Tu <vphatmail@gmail.com>
2023-11-12 02:48:08 +0000

    More websocket

--- frontend/src/pages/HomePage.tsx
+++ frontend/src/pages/HomePage.tsx
@@ -1,26 +1,19 @@
 import * as React from 'react'
-import {useEffect, useRef, useState} from 'react'
+import {useEffect, useState} from 'react'
 import '../css/HomePage.css'
 import {io, Socket} from "socket.io-client";
-import {useStrictMode} from "react-konva";
-import GridCanvas from '../components/GridCanvas.tsx';
 import ButtonAppBar from '../components/ButtonAppBar.tsx';
 import BasicSpeedDial from '../components/BasicSpeedDial.tsx';
 import Grid from "@mui/system/Unstable_Grid";
 import VirtualGrid from "../components/VirtualGrid.tsx";
 
-const gridSize = 100;
-
 function HomePage() {
-    useStrictMode(true);
 
-    const [socketio, setSocketio] = useState<Socket<never, never>>();
-    const canvasRef = useRef(null);
+    const [socketio, setSocketio] = useState<Socket<any, any>>();
     useEffect(() => {
-        const socket = io('localhost:8080')
+        const socket = io('127.0.0.1:3030')
         setSocketio(socket)
-
-        socket.on("test", (data) => {
+        socket.on("color_change", (data) => {
             console.log(data)
         })
         socket.on("disconnect", (data) => {
@@ -36,12 +29,9 @@ function HomePage() {
         <>
             <Grid container>
                 <Grid xs={12}>
-                    <ButtonAppBar/>
+                    <ButtonAppBar socketio={socketio}/>
                 </Grid>
                 <Grid xs={12}>
-                    {/*<GridCanvas width={innerWidth - (innerWidth * 0.2)} height={innerHeight} borderSize={0.5}*/}
-                    {/*            grid={Array.from(Array(gridSize), _ => Array(gridSize).fill('white'))}*/}
-                    {/*            cellSize={10}/>*/}
                     <VirtualGrid/>
                     <BasicSpeedDial/>
                 </Grid>
