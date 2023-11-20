11effba26c0b64367e6ce23f6b7d0e63a55b2cad
Vinh Tu <vphatmail@gmail.com>
2023-11-20 16:13:24 +0000

    more tersting

--- frontend/src/components/GridComponent.tsx
+++ frontend/src/components/GridComponent.tsx
@@ -1,61 +1,61 @@
-import React, {useEffect, useRef, useState} from 'react';
-import {Container, Graphics, Stage} from '@pixi/react';
-import * as PIXI from 'pixi.js';
-import Viewport from './Viewport';
-import {useSocketStore} from "../store/socketStore.ts";
-
-const GridComponent: React.FC = () => {
-    const viewportRef = useRef<any | null>(null);
-    const [isViewportReady, setViewportReady] = useState(false);
-
-    const gridSize = 1000;
-    const cellSize = 10;
-    const borderSize = 0.5; // Adjust the border size as needed
-    const gridRows = gridSize / cellSize;
-    const gridCols = gridSize / cellSize;
-
-    const gridColors = useSocketStore((state) => state.grid);
-
-    useEffect(() => {
-        if (viewportRef.current) {
-            setViewportReady(true);
-        }
-    }, [viewportRef.current]);
-
-    const renderGrid = (graphics: PIXI.Graphics) => {
-        if (!isViewportReady) {
-            return null; // or some loading state/rendering
-        }
-        if (gridColors.length === 0) {
-            return null;
-        }
-        const zoom = viewportRef.current.scale.x;
-        const interval = cellSize * zoom;
-
-        for (let i = 0; i < gridRows; i++) {
-            for (let j = 0; j < gridCols; j++) {
-                const x = i * interval;
-                const y = j * interval;
-
-                graphics.lineStyle(borderSize, 0x000000, 1); // Set the border color and size
-                graphics.beginFill(gridColors[j][i]);
-                graphics.drawRect(x, y, interval, interval);
-                graphics.endFill();
-            }
-        }
-        graphics.position.x = (innerWidth / 2) - (graphics.width / 2);
-        graphics.position.y = (innerHeight / 2) - (graphics.height / 2);
-    };
-
-    return (
-        <Stage width={innerWidth} height={800} options={{backgroundColor: 0xffffff}}>
-            <Viewport ref={viewportRef} width={innerWidth} height={800}>
-                <Container>
-                    <Graphics draw={renderGrid}/>
-                </Container>
-            </Viewport>
-        </Stage>
-    );
-};
-
-export default GridComponent;
+// import React, {useEffect, useRef, useState} from 'react';
+// import {Container, Graphics, Stage} from '@pixi/react';
+// import * as PIXI from 'pixi.js';
+// import Viewport from './Viewport';
+// import {useSocketStore} from "../store/socketStore.ts";
+//
+// const GridComponent: React.FC = () => {
+//     const viewportRef = useRef<any | null>(null);
+//     const [isViewportReady, setViewportReady] = useState(false);
+//
+//     const gridSize = 1000;
+//     const cellSize = 10;
+//     const borderSize = 0.5; // Adjust the border size as needed
+//     const gridRows = gridSize / cellSize;
+//     const gridCols = gridSize / cellSize;
+//
+//     const gridColors = useSocketStore((state) => state.grid);
+//
+//     useEffect(() => {
+//         if (viewportRef.current) {
+//             setViewportReady(true);
+//         }
+//     }, [viewportRef.current]);
+//
+//     const renderGrid = (graphics: PIXI.Graphics) => {
+//         if (!isViewportReady) {
+//             return null; // or some loading state/rendering
+//         }
+//         if (gridColors.length === 0) {
+//             return null;
+//         }
+//         const zoom = viewportRef.current.scale.x;
+//         const interval = cellSize * zoom;
+//
+//         for (let i = 0; i < gridRows; i++) {
+//             for (let j = 0; j < gridCols; j++) {
+//                 const x = i * interval;
+//                 const y = j * interval;
+//
+//                 graphics.lineStyle(borderSize, 0x000000, 1); // Set the border color and size
+//                 graphics.beginFill(gridColors[j][i]);
+//                 graphics.drawRect(x, y, interval, interval);
+//                 graphics.endFill();
+//             }
+//         }
+//         graphics.position.x = (innerWidth / 2) - (graphics.width / 2);
+//         graphics.position.y = (innerHeight / 2) - (graphics.height / 2);
+//     };
+//
+//     return (
+//         <Stage width={innerWidth} height={800} options={{backgroundColor: 0xffffff}}>
+//             <Viewport ref={viewportRef} width={innerWidth} height={800}>
+//                 <Container>
+//                     <Graphics draw={renderGrid}/>
+//                 </Container>
+//             </Viewport>
+//         </Stage>
+//     );
+// };
+//
+// export default GridComponent;
--- frontend/src/components/Viewport.tsx
+++ frontend/src/components/Viewport.tsx
@@ -1,62 +1,62 @@
-import React, {forwardRef} from "react";
-import * as PIXI from "pixi.js";
-import {EventSystem} from "pixi.js";
-import {PixiComponent, useApp} from "@pixi/react";
-import {Viewport as PixiViewport} from "pixi-viewport";
-import {Container as PixiContainer} from "@pixi/display";
-
-export interface ViewportProps {
-    width: number;
-    height: number;
-    children?: React.ReactNode;
-}
-
-export interface PixiComponentViewportProps extends ViewportProps {
-    app: PIXI.Application;
-}
-
-const PixiComponentViewport = PixiComponent("Viewport", {
-    create: (props: PixiComponentViewportProps) => {
-
-        // comes from github issue: https://github.com/davidfig/pixi-viewport/issues/438
-        // Install EventSystem, if not already
-        // (PixiJS 6 doesn't add it by default)
-
-
-        const events = new EventSystem(props.app.renderer);
-        events.domElement = props.app.renderer.view as any;
-
-        const viewport = new PixiViewport({
-            screenWidth: props.app.screen.width,
-            screenHeight: props.app.screen.height,
-            worldWidth: props.width,
-            worldHeight: props.height,
-            ticker: props.app.ticker,
-            events: events,
-        });
-        viewport.drag().pinch().wheel().clampZoom({
-            minWidth: 10,
-            minHeight: 10,
-            maxWidth: 10000,
-            maxHeight: 10000,
-        });
-
-        return viewport;
-    },
-
-    willUnmount: (instance: PixiViewport, parent: PixiContainer) => {
-        // workaround because the ticker is already destroyed by this point by the stage
-        instance.options.noTicker = true;
-        instance.destroy({children: true, texture: true, baseTexture: true})
-
-    }
-});
-
-const Viewport = forwardRef(
-    (props: ViewportProps, ref: React.Ref<PixiViewport>) => {
-        const app = useApp();
-        return <PixiComponentViewport ref={ref} app={app} {...props} />;
-    }
-);
-
-export default Viewport;
+// import React, {forwardRef} from "react";
+// import * as PIXI from "pixi.js";
+// import {EventSystem} from "pixi.js";
+// import {PixiComponent, useApp} from "@pixi/react";
+// import {Viewport as PixiViewport} from "pixi-viewport";
+// import {Container as PixiContainer} from "@pixi/display";
+//
+// export interface ViewportProps {
+//     width: number;
+//     height: number;
+//     children?: React.ReactNode;
+// }
+//
+// export interface PixiComponentViewportProps extends ViewportProps {
+//     app: PIXI.Application;
+// }
+//
+// const PixiComponentViewport = PixiComponent("Viewport", {
+//     create: (props: PixiComponentViewportProps) => {
+//
+//         // comes from github issue: https://github.com/davidfig/pixi-viewport/issues/438
+//         // Install EventSystem, if not already
+//         // (PixiJS 6 doesn't add it by default)
+//
+//
+//         const events = new EventSystem(props.app.renderer);
+//         events.domElement = props.app.renderer.view as any;
+//
+//         const viewport = new PixiViewport({
+//             screenWidth: props.app.screen.width,
+//             screenHeight: props.app.screen.height,
+//             worldWidth: props.width,
+//             worldHeight: props.height,
+//             ticker: props.app.ticker,
+//             events: events,
+//         });
+//         viewport.drag().pinch().wheel().clampZoom({
+//             minWidth: 10,
+//             minHeight: 10,
+//             maxWidth: 10000,
+//             maxHeight: 10000,
+//         });
+//
+//         return viewport;
+//     },
+//
+//     willUnmount: (instance: PixiViewport, parent: PixiContainer) => {
+//         // workaround because the ticker is already destroyed by this point by the stage
+//         instance.options.noTicker = true;
+//         instance.destroy({children: true, texture: true, baseTexture: true})
+//
+//     }
+// });
+//
+// const Viewport = forwardRef(
+//     (props: ViewportProps, ref: React.Ref<PixiViewport>) => {
+//         const app = useApp();
+//         return <PixiComponentViewport ref={ref} app={app} {...props} />;
+//     }
+// );
+//
+// export default Viewport;
