f9fcbad6232d0484082d029498417f26cc16cd00
Vinh Tu <vphatmail@gmail.com>
2023-11-24 11:18:21 +0000

    added lecturer deploy script and modified them

--- killProcesses.sh
+++ killProcesses.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-
 # Kill pre-existing processes
 NODEPID=$(lsof -t -i :5173 -s TCP:LISTEN)
 kill -9 "$NODEPID"
@@ -9,8 +8,3 @@ kill -9 "$FLASKPID"
 
 DBPID=$(lsof -t -i :8090 -s TCP:LISTEN)
 kill -9 "$DBPID"
-
-source backend/venv/bin/activate
-
-# Start processes
-./db/pocketbase serve & python ./backend/app.py & pnpm --prefix ./frontend run preview --host --port 5173
